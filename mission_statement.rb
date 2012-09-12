e2102babe78c113ccf39a5ea04e4f55eb071b996
Aayush Iyer <aayush.iyer@gmail.com>
2012-09-12 15:31:36 -0600

    Mission Statement

---
+++ mission_statement.rb
@@ -0,0 +1,8 @@
+puts <<MYSTATEMENT
+I've worked in advertising for far too long, and I've worked with design, copy and strategy.
+However, I started off as an engineer and have always been fascinated with code (starting off 
+with C and 8085 micro-processor programming).
+For this reason, I'll love to have a goal to eventually become a full-stack 
+developer who can prototype ideas and make them come to life. 
+Hopefully.
+MYSTATEMENT
