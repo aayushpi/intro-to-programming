595ac2bf3a65e23b982910bfc95ac2167504d1bf
Aayush Iyer <aayush.iyer@gmail.com>
2012-09-12 16:20:55 -0600

    If case

--- mission_statement.rb
+++ mission_statement.rb
@@ -1,8 +1,18 @@
-puts <<MYSTATEMENT
-I've worked in advertising for far too long, and I've worked with design, copy and strategy.
-However, I started off as an engineer and have always been fascinated with code (starting off 
-with C and 8085 micro-processor programming).
-For this reason, I'll love to have a goal to eventually become a full-stack 
-developer who can prototype ideas and make them come to life. 
-Hopefully.
-MYSTATEMENT
+puts "Enter 1 for the inspiring answer, and 2 for the real answer"
+
+choice = gets.chomp()
+
+if choice == 1
+     puts <<MYSTATEMENT
+	I've worked in advertising for far too long, and I've worked with design, copy and strategy.
+	However, I started off as an engineer and have always been fascinated with code (starting off 
+	with C and 8085 micro-processor programming).
+	For this reason, I'll love to have a goal to eventually become a full-stack 
+	developer who can prototype ideas and make them come to life. 
+	Hopefully.
+	MYSTATEMENT
+elsif choice == 2
+	puts "ONE BILLION DOLLARS (DRAMATIC DR.EVIL POSE"
+else
+	puts "You only have two choices, Mr. Anderson"		
+end
