a81b2b0ac24095e407b536edc03a9d575fa0f7f1
Aayush Iyer <aayush.iyer@gmail.com>
2012-09-19 13:15:06 -0600

    Changes

--- mission_statement.rb
+++ mission_statement.rb
@@ -5,24 +5,26 @@ choice = gets.chomp()
 # puts "#{choice}"
 
 if choice == "1"
-    puts <<MYSTATEMENT
+  puts <<MYSTATEMENT
 	I've worked in advertising for far too long, and I've worked with design, copy and strategy.
 	However, I started off as an engineer and have always been fascinated with code (starting off 
 	with C and 8085 micro-processor programming).
 	For this reason, I'll love to have a goal to eventually become a full-stack 
 	developer who can prototype ideas and make them come to life. 
 	Hopefully.
+
+	I also want world peace and free range kittens.
 MYSTATEMENT
 
 elsif choice == "2"
 	for i in 0..100000
-   puts "$500 $500 $500 $500 $500"
-   puts "$500 $500 $500 $500 "
-   puts "$500 $500 $500 $500 $500"
-   puts "$500 $500 $500 $500 $500 $500"
+  puts "$500 $500 $500 $500 $500"
+  puts "$500 $500 $500 $500 "
+  puts "$500 $500 $500 $500 $500"
+	puts "$500 $500 $500 $500 $500 $500"
 end
 	puts "ONE BILLION DOLLARS (DRAMATIC DR.EVIL POSE). So yes, I'd love to learn to code hardcore, please"
 
 else
-	puts "You only have two choices, Mr. Anderson"		
+	puts "You only have two choices, Mr. Anderson"
 end
