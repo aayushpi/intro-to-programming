f5a22c37f6f67a1ad6f5fe64054d0e3d91fadb91
Aayush Iyer <aayush.iyer@gmail.com>
2012-09-20 10:02:57 -0600

    sup

--- doomsday.rb
+++ doomsday.rb
@@ -1,20 +1,25 @@
+#Defining new year and current time
 new_year = Time.local(2013, "Jan", 1,0,0,0)	
 current = Time.now
+
+#Creating a difference between the timestamps
 diff= new_year-current
+
+
+
+#converting the timestamp to an integer
 temp=diff.to_i
-new_time=Time.at(temp)
 
-puts new_time.inspect
-
-puts "In human years, it's #{new_time.month} months, #{new_time.day} days, #{new_time.hour} hour, #{new_time.min} and #{new_time.sec} seconds till the new year! Woot!"
-
-dog_temp=diff * 7
+#Multiplying original difference by 7
+dog_temp=temp * 7
 dog_timestamp=dog_temp.to_i
-
 dog_time=Time.at(dog_timestamp)
 
 
+#recreating a new object.
+new_time=Time.at(temp)
 
-puts dog_time
+#Human years :: Extracting the properties of the object.
+puts "In human years, it's #{new_time.month} months, #{new_time.day} days, #{new_time.hour} hour, #{new_time.min} and #{new_time.sec} seconds till the new year! Woot!"
 
 puts "In dog years, it's #{dog_time.year-1970} years, #{dog_time.month} months, #{dog_time.day} days, #{dog_time.hour} hour, #{dog_time.min} and #{dog_time.sec} seconds till the new year! Woof!"
--- kickstarterfees.rb
+++ kickstarterfees.rb
@@ -1,10 +1,12 @@
 total_collections = 305000000
 
-launched_on = Time.local(2009, "Apr", 28)	
+launched_on = Time.local(2009, "Apr", 28)
 
 current=Time.now
 
 kick_fee = total_collections * 0.05
+
+puts kick_fee
 
 diff= current - launched_on
 
